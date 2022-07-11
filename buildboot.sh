fe97ccdc7abfedefee40efacecf8d88228b50d31
123markhong <2386712909@qq.com>
2022-07-10 22:48:13 -0700

    origin version

---
+++ buildboot.sh
@@ -0,0 +1,11 @@
+source /opt/imx6ul/environment-setup-cortexa7t2hf-neon-poky-linux-gnueabi
+make distclean
+make myd_imx6ull_nand_ddr256_defconfig
+#make myd_imx6ul_emmc_defconfig 
+#make myd_imx6ul_nand_ddr256_defconfig
+#make myd_imx6ull_emmc_defconfig
+make  -j16
+
+#mkdir -p $PWD/../build
+#cp $PWD/arch/arm/boot/zImage $PWD/../build/
+#cp $PWD/arch/arm/boot/dts/myd-y6ull*.dtb $PWD/../build/
